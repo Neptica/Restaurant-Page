51ff62419c0207fff87084470181bdf0206ff201
Michael Mumme <neptica@hotmail.com>
2024-11-05 23:48:59 -0600

    add navbar update functionality

---
+++ src/about.js
@@ -0,0 +1,6 @@
+export default (function () {
+  const content = document.getElementById("content");
+  const p = document.createElement("h1");
+  p.textContent = "Herro";
+  content.appendChild(p);
+});
--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import "./styles.css";
-console.log("Hello");
+import "./style.css";
+import home from "./home.js";
+import menu from "./menu.js";
+import locations from "./locations.js";
+import about from "./about.js";
+
+(function () {
+  const navbar = document.getElementById("navbar");
+  const functions = [home, menu, locations, about];
+  const children = navbar.children;
+  for (let i = 0; i < children.length; i++) {
+    console.log("hello");
+    console.log[functions[i]];
+    children[i].addEventListener("click", functions[i]);
+  }
+})();
